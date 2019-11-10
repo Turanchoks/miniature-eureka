d8a8766499297376adfea958bc0d35970a9bcf78
KateKate <anishkina@gmail.com>
2019-11-10 23:57:49 +0300

    fixtures chat list

---
+++ src/fixtures.js
@@ -0,0 +1,503 @@
+var chatList = [
+  {
+    _: "chat",
+    id: 777000,
+    type: {
+      _: "chatTypePrivate",
+      userId: 777000
+    },
+    title: "Telegram",
+    photo: {
+      _: "chatPhoto",
+      small: {
+        _: "file",
+        id: 3,
+        size: 0,
+        expectedSize: 0,
+        local: {
+          _: "localFile",
+          path: "",
+          canBeDownloaded: true,
+          canBeDeleted: false,
+          isDownloadingActive: false,
+          isDownloadingCompleted: false,
+          downloadOffset: 0,
+          downloadedPrefixSize: 0,
+          downloadedSize: 0
+        },
+        remote: {
+          _: "remoteFile",
+          id: "AQADAQADr6cxGyjbCwAJRPdrBgAEAgADKNsLAAUxOMuh5xKT0qozAAIWBA",
+          isUploadingActive: false,
+          isUploadingCompleted: true,
+          uploadedSize: 0
+        }
+      },
+      big: {
+        _: "file",
+        id: 4,
+        size: 0,
+        expectedSize: 0,
+        local: {
+          _: "localFile",
+          path: "",
+          canBeDownloaded: true,
+          canBeDeleted: false,
+          isDownloadingActive: false,
+          isDownloadingCompleted: false,
+          downloadOffset: 0,
+          downloadedPrefixSize: 0,
+          downloadedSize: 0
+        },
+        remote: {
+          _: "remoteFile",
+          id: "AQADAQADr6cxGyjbCwAJRPdrBgAEAwADKNsLAAUxOMuh5xKT0qwzAAIWBA",
+          isUploadingActive: false,
+          isUploadingCompleted: true,
+          uploadedSize: 0
+        }
+      }
+    },
+    permissions: {
+      _: "chatPermissions",
+      canSendMessages: true,
+      canSendMediaMessages: true,
+      canSendPolls: true,
+      canSendOtherMessages: true,
+      canAddWebPagePreviews: true,
+      canChangeInfo: false,
+      canInviteUsers: false,
+      canPinMessages: false
+    },
+    lastMessage: {
+      _: "message",
+      id: 63559434240,
+      senderUserId: 777000,
+      chatId: 777000,
+      isOutgoing: false,
+      canBeEdited: false,
+      canBeForwarded: true,
+      canBeDeletedOnlyForSelf: true,
+      canBeDeletedForAllUsers: true,
+      isChannelPost: false,
+      containsUnreadMention: false,
+      date: 1573415487,
+      editDate: 0,
+      replyToMessageId: 0,
+      ttl: 0,
+      ttlExpiresIn: 0,
+      viaBotUserId: 0,
+      authorSignature: "",
+      views: 0,
+      mediaAlbumId: "0",
+      content: {
+        _: "messageText",
+        text: {
+          _: "formattedText",
+          text:
+            "Login code: 38635. Do not give this code to anyone, even if they say they are from Telegram!\n\nThis code can be used to log in to your Telegram account. We never ask it for anything else.\n\nIf you didn't request this code by trying to log in on another device, simply ignore this message.",
+          entities: [
+            {
+              _: "textEntity",
+              offset: 0,
+              length: 11,
+              type: {
+                _: "textEntityTypeBold"
+              }
+            },
+            {
+              _: "textEntity",
+              offset: 22,
+              length: 3,
+              type: {
+                _: "textEntityTypeBold"
+              }
+            }
+          ]
+        }
+      }
+    },
+    order: "6757768059684973767",
+    isPinned: false,
+    isMarkedAsUnread: false,
+    isSponsored: false,
+    canBeDeletedOnlyForSelf: true,
+    canBeDeletedForAllUsers: true,
+    canBeReported: false,
+    defaultDisableNotification: false,
+    unreadCount: 1,
+    lastReadInboxMessageId: 63475548160,
+    lastReadOutboxMessageId: 0,
+    unreadMentionCount: 0,
+    notificationSettings: {
+      _: "chatNotificationSettings",
+      useDefaultMuteFor: true,
+      muteFor: 0,
+      useDefaultSound: true,
+      sound: "default",
+      useDefaultShowPreview: true,
+      showPreview: false,
+      useDefaultDisablePinnedMessageNotifications: true,
+      disablePinnedMessageNotifications: false,
+      useDefaultDisableMentionNotifications: true,
+      disableMentionNotifications: false
+    },
+    pinnedMessageId: 0,
+    replyMarkupMessageId: 0,
+    clientData: "",
+    "@extra": {
+      query_id: 9
+    }
+  },
+
+  {
+    _: "chat",
+    id: -1001322215945,
+    type: {
+      _: "chatTypeSupergroup",
+      supergroupId: 1322215945,
+      isChannel: false
+    },
+    title: "Telegram Contests",
+    photo: {
+      _: "chatPhoto",
+      small: {
+        _: "file",
+        id: 103,
+        size: 0,
+        expectedSize: 0,
+        local: {
+          _: "localFile",
+          path: "",
+          canBeDownloaded: true,
+          canBeDeleted: false,
+          isDownloadingActive: false,
+          isDownloadingCompleted: false,
+          downloadOffset: 0,
+          downloadedPrefixSize: 0,
+          downloadedSize: 0
+        },
+        remote: {
+          _: "remoteFile",
+          id: "AQADAQATVyAUMAAEAgAD94WL3Bb___-q4kyBaMvphU-bAQABFgQ",
+          isUploadingActive: false,
+          isUploadingCompleted: true,
+          uploadedSize: 0
+        }
+      },
+      big: {
+        _: "file",
+        id: 104,
+        size: 0,
+        expectedSize: 0,
+        local: {
+          _: "localFile",
+          path: "",
+          canBeDownloaded: true,
+          canBeDeleted: false,
+          isDownloadingActive: false,
+          isDownloadingCompleted: false,
+          downloadOffset: 0,
+          downloadedPrefixSize: 0,
+          downloadedSize: 0
+        },
+        remote: {
+          _: "remoteFile",
+          id: "AQADAQATVyAUMAAEAwAD94WL3Bb___-q4kyBaMvphVGbAQABFgQ",
+          isUploadingActive: false,
+          isUploadingCompleted: true,
+          uploadedSize: 0
+        }
+      }
+    },
+    permissions: {
+      _: "chatPermissions",
+      canSendMessages: true,
+      canSendMediaMessages: true,
+      canSendPolls: true,
+      canSendOtherMessages: true,
+      canAddWebPagePreviews: true,
+      canChangeInfo: false,
+      canInviteUsers: true,
+      canPinMessages: false
+    },
+    lastMessage: {
+      _: "message",
+      id: 58608058368,
+      senderUserId: 468527313,
+      chatId: -1001322215945,
+      isOutgoing: false,
+      canBeEdited: false,
+      canBeForwarded: true,
+      canBeDeletedOnlyForSelf: false,
+      canBeDeletedForAllUsers: false,
+      isChannelPost: false,
+      containsUnreadMention: false,
+      date: 1573417653,
+      editDate: 0,
+      replyToMessageId: 58596524032,
+      ttl: 0,
+      ttlExpiresIn: 0,
+      viaBotUserId: 0,
+      authorSignature: "",
+      views: 0,
+      mediaAlbumId: "0",
+      content: {
+        _: "messageText",
+        text: {
+          _: "formattedText",
+          text:
+            "There are much more entries for the theme contest and it's subjective anyway, as in, there's no purely technical way to decide which themes are better.",
+          entities: []
+        }
+      }
+    },
+    order: "6757777362584132181",
+    isPinned: false,
+    isMarkedAsUnread: false,
+    isSponsored: false,
+    canBeDeletedOnlyForSelf: false,
+    canBeDeletedForAllUsers: false,
+    canBeReported: true,
+    defaultDisableNotification: false,
+    unreadCount: 4,
+    lastReadInboxMessageId: 58597572608,
+    lastReadOutboxMessageId: 58608058368,
+    unreadMentionCount: 0,
+    notificationSettings: {
+      _: "chatNotificationSettings",
+      useDefaultMuteFor: false,
+      muteFor: 574065973,
+      useDefaultSound: true,
+      sound: "default",
+      useDefaultShowPreview: true,
+      showPreview: false,
+      useDefaultDisablePinnedMessageNotifications: true,
+      disablePinnedMessageNotifications: false,
+      useDefaultDisableMentionNotifications: true,
+      disableMentionNotifications: false
+    },
+    pinnedMessageId: 53657731072,
+    replyMarkupMessageId: 0,
+    clientData: "",
+    "@extra": {
+      query_id: 6
+    }
+  },
+
+  {
+    _: "chat",
+    id: -378741822,
+    type: {
+      _: "chatTypeBasicGroup",
+      basicGroupId: 378741822
+    },
+    title: "Хуярим TG Contest",
+    permissions: {
+      _: "chatPermissions",
+      canSendMessages: true,
+      canSendMediaMessages: true,
+      canSendPolls: true,
+      canSendOtherMessages: true,
+      canAddWebPagePreviews: true,
+      canChangeInfo: true,
+      canInviteUsers: true,
+      canPinMessages: true
+    },
+    lastMessage: {
+      _: "message",
+      id: 63681069056,
+      senderUserId: 98479329,
+      chatId: -378741822,
+      isOutgoing: false,
+      canBeEdited: false,
+      canBeForwarded: true,
+      canBeDeletedOnlyForSelf: true,
+      canBeDeletedForAllUsers: false,
+      isChannelPost: false,
+      containsUnreadMention: false,
+      date: 1573417364,
+      editDate: 0,
+      replyToMessageId: 0,
+      ttl: 0,
+      ttlExpiresIn: 0,
+      viaBotUserId: 0,
+      authorSignature: "",
+      views: 0,
+      mediaAlbumId: "0",
+      content: {
+        _: "messageText",
+        text: {
+          _: "formattedText",
+          text: "я вспомнил)",
+          entities: []
+        }
+      }
+    },
+    order: "6757776121338588475",
+    isPinned: false,
+    isMarkedAsUnread: false,
+    isSponsored: false,
+    canBeDeletedOnlyForSelf: true,
+    canBeDeletedForAllUsers: false,
+    canBeReported: false,
+    defaultDisableNotification: false,
+    unreadCount: 0,
+    lastReadInboxMessageId: 63681069056,
+    lastReadOutboxMessageId: 63670583296,
+    unreadMentionCount: 0,
+    notificationSettings: {
+      _: "chatNotificationSettings",
+      useDefaultMuteFor: true,
+      muteFor: 0,
+      useDefaultSound: true,
+      sound: "default",
+      useDefaultShowPreview: true,
+      showPreview: false,
+      useDefaultDisablePinnedMessageNotifications: true,
+      disablePinnedMessageNotifications: false,
+      useDefaultDisableMentionNotifications: true,
+      disableMentionNotifications: false
+    },
+    pinnedMessageId: 0,
+    replyMarkupMessageId: 0,
+    clientData: "",
+    "@extra": {
+      query_id: 8
+    }
+  },
+
+  {
+    _: "chat",
+    id: -1001142273288,
+    type: {
+      _: "chatTypeSupergroup",
+      supergroupId: 1142273288,
+      isChannel: false
+    },
+    title: "Svelte [svelt]",
+    photo: {
+      _: "chatPhoto",
+      small: {
+        _: "file",
+        id: 105,
+        size: 0,
+        expectedSize: 0,
+        local: {
+          _: "localFile",
+          path: "",
+          canBeDownloaded: true,
+          canBeDeleted: false,
+          isDownloadingActive: false,
+          isDownloadingCompleted: false,
+          downloadOffset: 0,
+          downloadedPrefixSize: 0,
+          downloadedSize: 0
+        },
+        remote: {
+          _: "remoteFile",
+          id: "AQADAgATB1s5DwAEAgAD-DpF5xb___-WJWTszvXeiVsQBAABFgQ",
+          isUploadingActive: false,
+          isUploadingCompleted: true,
+          uploadedSize: 0
+        }
+      },
+      big: {
+        _: "file",
+        id: 106,
+        size: 0,
+        expectedSize: 0,
+        local: {
+          _: "localFile",
+          path: "",
+          canBeDownloaded: true,
+          canBeDeleted: false,
+          isDownloadingActive: false,
+          isDownloadingCompleted: false,
+          downloadOffset: 0,
+          downloadedPrefixSize: 0,
+          downloadedSize: 0
+        },
+        remote: {
+          _: "remoteFile",
+          id: "AQADAgATB1s5DwAEAwAD-DpF5xb___-WJWTszvXeiV0QBAABFgQ",
+          isUploadingActive: false,
+          isUploadingCompleted: true,
+          uploadedSize: 0
+        }
+      }
+    },
+    permissions: {
+      _: "chatPermissions",
+      canSendMessages: true,
+      canSendMediaMessages: true,
+      canSendPolls: true,
+      canSendOtherMessages: true,
+      canAddWebPagePreviews: true,
+      canChangeInfo: false,
+      canInviteUsers: true,
+      canPinMessages: false
+    },
+    lastMessage: {
+      _: "message",
+      id: 48229253120,
+      senderUserId: 308133687,
+      chatId: -1001142273288,
+      isOutgoing: false,
+      canBeEdited: false,
+      canBeForwarded: true,
+      canBeDeletedOnlyForSelf: false,
+      canBeDeletedForAllUsers: false,
+      isChannelPost: false,
+      containsUnreadMention: false,
+      date: 1573417485,
+      editDate: 0,
+      replyToMessageId: 0,
+      ttl: 0,
+      ttlExpiresIn: 0,
+      viaBotUserId: 0,
+      authorSignature: "",
+      views: 0,
+      mediaAlbumId: "0",
+      content: {
+        _: "messageText",
+        text: {
+          _: "formattedText",
+          text: "пока еще не документировали",
+          entities: []
+        }
+      }
+    },
+    order: "6757776641029616555",
+    isPinned: false,
+    isMarkedAsUnread: false,
+    isSponsored: false,
+    canBeDeletedOnlyForSelf: false,
+    canBeDeletedForAllUsers: false,
+    canBeReported: true,
+    defaultDisableNotification: false,
+    unreadCount: 644,
+    lastReadInboxMessageId: 47536144384,
+    lastReadOutboxMessageId: 48229253120,
+    unreadMentionCount: 0,
+    notificationSettings: {
+      _: "chatNotificationSettings",
+      useDefaultMuteFor: false,
+      muteFor: 574065973,
+      useDefaultSound: true,
+      sound: "default",
+      useDefaultShowPreview: true,
+      showPreview: false,
+      useDefaultDisablePinnedMessageNotifications: true,
+      disablePinnedMessageNotifications: false,
+      useDefaultDisableMentionNotifications: true,
+      disableMentionNotifications: false
+    },
+    pinnedMessageId: 48221913088,
+    replyMarkupMessageId: 0,
+    clientData: "",
+    "@extra": {
+      query_id: 7
+    }
+  }
+];
