314c1b95c6ac94c5c3ff8d8a6b5be4f0cc62a4f1
kostenko-anton <kostenko-anton@users.noreply.github.com>
2019-11-10 15:46:31 +0300

    country code dropdown

--- src/country-code-select/select.js
+++ src/country-code-select/select.js
@@ -1,3 +1,5 @@
+let selected = null;
+
 const toggleDropdown = () => {
   const dropdown = document.querySelector('.country-code-view');
   dropdown.classList.toggle('hide');
@@ -6,7 +8,19 @@ const toggleDropdown = () => {
   input.classList.toggle('active');
 };
 
+const handleChange = country => {
+  const placeholder = document.querySelector(
+    '.country-code-select__placeholder'
+  );
+  selected = country;
+  if (placeholder) {
+    placeholder.classList.add('selected');
+    placeholder.textContent = country.name;
+  }
+};
+
 const select = () => {
+  const selected = null;
   const selectViewParent = document.getElementById('main');
 
   //fake input
@@ -18,8 +32,13 @@ const select = () => {
   const inputPlaceholder = document.createElement('div');
   inputPlaceholder.classList = 'country-code-select__placeholder';
   inputPlaceholder.textContent = 'Country';
-  // inputPlaceholder.appendChild(dropdownIcon());
   selectInput.appendChild(inputPlaceholder);
+
+  //icon
+  const dropdownIcon = document.createElement('div');
+  dropdownIcon.classList = 'country-code-select__icon';
+  dropdownIcon.textContent = '>';
+  selectInput.appendChild(dropdownIcon);
 
   selectViewParent.appendChild(selectInput);
 
@@ -30,10 +49,17 @@ const select = () => {
     const optionEl = document.createElement('div');
     optionEl.classList = 'country-code-view__option';
     const flagEl = document.createElement('span');
-    flagEl.setAttribute('class', 'country-code-view__option--flag');
     flagEl.textContent = country.emoji;
     optionEl.appendChild(flagEl);
-    optionEl.insertAdjacentHTML('beforeend', country.name);
+    optionEl.insertAdjacentHTML(
+      'beforeend',
+      `<span class="country-name">${country.name}</span>`
+    );
+    optionEl.insertAdjacentHTML(
+      'beforeend',
+      `<span class="dialling-code">${country.diallingCode}</span>`
+    );
+    optionEl.addEventListener('click', () => handleChange(country));
     selectView.appendChild(optionEl);
   });
   selectViewParent.appendChild(selectView);
