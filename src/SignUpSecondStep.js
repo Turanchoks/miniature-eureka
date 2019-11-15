4bf87ff74b498c599c63153d60f8e533552b1c7e
KateKate <anishkina@gmail.com>
2019-11-15 13:20:36 +0300

    fix Monkey

--- src/SignUpSecondStep.js
+++ src/SignUpSecondStep.js
@@ -1,6 +1,15 @@
 import { setState, setLoading } from "./state";
 import { apiClient, initUpdateApiData } from "./api";
+import TwoFactorSetupMonkeyIdle from "./Monkey/TwoFactorSetupMonkeyIdle.tgs";
+import TwoFactorSetupMonkeyTracking from "./Monkey/TwoFactorSetupMonkeyTracking.tgs";
 import { h } from "./superfine";
+
+const valid = value => {
+  const player = document.querySelector("tgs-player");
+  const sts = !value;
+  const monkey = sts ? TwoFactorSetupMonkeyIdle : TwoFactorSetupMonkeyTracking;
+  player.load(monkey);
+};
 
 const handleCodeChange = e => {
   setState({
