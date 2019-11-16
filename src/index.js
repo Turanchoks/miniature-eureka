5c4f6e3a8ff2e5b8ee68be0371922945bfc0a3c6
KateKate <anishkina@gmail.com>
2019-11-16 23:53:20 +0300

    import apiClient

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // import { patch as render, h } from "./superfine-raw";
 import { state } from "./state";
 import { renderApp } from "./renderApp";
+import { apiClient } from "./apiClient";
 
 renderApp(state);
