3a905a17b34d1b713e606bfa33e1f3e161c48181
Pavel Turanchoks <ipuncho@gmail.com>
2019-11-16 15:59:39 +0300

    dynamic import App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import { h } from "./superfine";
-import { SignUpFirstStep } from "./SignUpFirstStep";
-import { SignUpSecondStep } from "./SignUpSecondStep";
-import { Chats } from "./Chats";
+import { h } from './superfine';
+import { setState } from './state';
+
+let requireUpdate = false;
+
+const nullRender = () => {
+  requireUpdate = true;
+  return <div />;
+};
+
+const errorRender = () => (
+  <h1>An error has occured. Please try reloading the page.</h1>
+);
+
+const ric = window.requestIdleCallback || (cb => setTimeout(cb, 0));
 
 const CheckingYourStatus = () => {
   return (
@@ -9,18 +20,57 @@ const CheckingYourStatus = () => {
   );
 };
 
+const Components = {
+  CheckingYourStatus,
+};
+
+const imports = [
+  [() => import('./SignUpFirstStep'), 'SignUpFirstStep'],
+  [() => import('./SignUpSecondStep'), 'SignUpSecondStep'],
+  [() => import('./Chats'), 'Chats'],
+];
+
+for (const [loader, name] of imports) {
+  Components[name] = nullRender;
+  ric(() =>
+    loader()
+      // test slow connection and nullRender
+      // .then(module => {
+      //   return new Promise(resolve => {
+      //     setTimeout(() => {
+      //       resolve(module);
+      //     }, 3000);
+      //   });
+      // })
+      .then(module => {
+        Components[name] = module[name];
+        console.log(requireUpdate);
+        if (requireUpdate) {
+          // force update
+          setState({
+            now: Date.now(),
+          });
+        }
+        requireUpdate = false;
+      })
+      .catch(() => {
+        Components[name] = errorRender;
+      })
+  );
+}
+
 export const App = state => {
   const { isAuthorized, loading, step } = state;
   // getAuthorizationState is an offline request
 
   switch (step) {
-    case "phone input":
-      return SignUpFirstStep(state);
-    case "check code":
-      return SignUpSecondStep(state);
-    case "chats":
-      return Chats(state);
+    case 'phone input':
+      return Components.SignUpFirstStep(state);
+    case 'check code':
+      return Components.SignUpSecondStep(state);
+    case 'chats':
+      return Components.Chats(state);
     default:
-      return CheckingYourStatus(state);
+      return Components.CheckingYourStatus(state);
   }
 };
